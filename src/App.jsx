34ac065a97901c2bb09112bfcde61152b493c990
BrenoRicardo <114194243+BrenoRicardo@users.noreply.github.com>
2023-01-18 16:26:55 -0300

    Login adicionado
    Sistema de login adicionado

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './App.css'
-import Header from './components/Header.jsx'
-import Home from './routes/Home.jsx'
-import Filmes from './routes/Filmes.jsx'
-import Sobre from './routes/Sobre.jsx'
-import Footer from './components/Footer.jsx'
-import PostFilme from './routes/PostFilme.jsx'
-import Contato from './routes/Contato.jsx'
+import { useEffect, useState } from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  redirect,
+  useNavigate,
+  Navigate,
+} from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header.jsx";
+import Home from "./routes/Home.jsx";
+import Filmes from "./routes/Filmes.jsx";
+import Sobre from "./routes/Sobre.jsx";
+import Footer from "./components/Footer.jsx";
+import PostFilme from "./routes/PostFilme.jsx";
+import Login from "./routes/Login.jsx";
+
 
 function App() {
 
@@ -15,15 +23,15 @@ function App() {
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route exact path='/' element={<Home />} />
-        <Route path='/Filmes' element={<Filmes />} />
-        <Route path='/Sobre' element={<Sobre />} />
-        <Route path='/PostarFilme' element={<PostFilme />}/>
-        <Route path='/Contato' element={<Contato />} />
+        <Route exact path="/" element={<Home />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Filmes" element={<Filmes />} />
+        <Route path="/Sobre" element={<Sobre />} />
+        <Route path="/PostarFilme" element={<PostFilme />} />
       </Routes>
       <Footer />
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;

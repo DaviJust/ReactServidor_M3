bce991aa726fa39d8e797396595f6a209e42ba9c
Davi <112717111+DaviJust@users.noreply.github.com>
2023-01-23 15:54:24 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,10 @@
-#!/usr/bin/env sh
-
-# abort on errors
 set -e
 
-# build
 npm run build
 
-# navigate into the build output directory
 cd Servidor_M3
 
-# place .nojekyll to bypass Jekyll processing
 echo > .nojekyll
-
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
 
 git init
 git checkout -B main
